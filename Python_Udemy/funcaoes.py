d764fd54d97b3d0d1891355259f1da562ec90f23
Altamirus <altamirus@hotmail.com>
2023-11-26 13:24:13 -0300

    funcoes_parametros_argumentos

---
+++ Python_Udemy/funcaoes.py
@@ -0,0 +1,33 @@
+# Dry - Don't repeat yourself
+# Funcions (Funcoes)
+
+'''
+def boas_vindas():
+
+    print('Ola Altamiro')
+    print('Temos 5 laptops em estoque')
+
+
+boas_vindas()
+
+
+def somar():
+    num1 = 10
+    num2 = 5
+    resultado = num1 + num2
+    print(resultado)
+
+
+somar()
+'''
+# Parametros e Argumentos
+
+
+def ola(nome, qtd):
+    print(f' Ola {nome}')
+    print(f' Temos {qtd} laptops em estoque')
+
+
+ola('Marcos', 5)
+ola('Maria', 10)
+ola('Miriam', 3)
