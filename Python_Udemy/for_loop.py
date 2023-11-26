59501d875b1e1df87d1f69911726792e21f7e198
Altamirus <altamirus@hotmail.com>
2023-11-26 19:55:39 -0300

    Loop dentro de uma lista

--- Python_Udemy/for_loop.py
+++ Python_Udemy/for_loop.py
@@ -1,8 +1,20 @@
+
+'''
 linha = 20
 coluna = 30
 simbolo = '#'
 
-for l in range(linha):
+for lin in range(linha):
     for c in range(coluna):
         print(simbolo, end='')
     print()
+'''
+
+
+# For loop dentro de uma lista
+
+valores = [50, 80, 110, 150, 170]
+
+for x in valores:
+
+    print(f' o valor final do produto e de R$ {x}.')
