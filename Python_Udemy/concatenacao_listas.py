1ee73452992869052d938d240e402b13e5f6719d
Altamirus <altamirus@hotmail.com>
2023-11-26 17:41:13 -0300

    concatenacao e listas

---
+++ Python_Udemy/concatenacao_listas.py
@@ -0,0 +1,17 @@
+# Listas
+# Armazenar mais de uma informacao em variaveis
+# Manter a sequencia ddos dados em uma variavel
+
+
+numeros = [2, 3, 4, 5]
+letras = ['a', 'b', 'c', 'd']
+
+por1 = numeros * 1
+por2 = numeros * 2
+por3 = numeros * 3
+porLetras = numeros + letras
+
+print(por1)
+print(por2)
+print(por3)
+print(porLetras)
---
+++ Python_Udemy/listas.py
@@ -0,0 +1,45 @@
+# Listas
+# Armazenar mais de uma informacao em variaveis
+# Manter a sequencia ddos dados em uma variavel
+
+# Exemplo-1
+cidade1 = 'Rio de Janeiro'
+cidade2 = 'Sao Paulo'
+cidade3 = 'Minas'
+
+# Exemplo-2
+cidades = ['Rio de Janeiro', 'Sao Paulo', 'Salvador', 'Goiana']
+
+# indices          0               1           2        3
+'''
+# imprimindo valor do indece 1
+print(cidades)
+print(cidades[0])
+print(cidades[3])
+
+
+# Trocando o valor do indece
+
+cidades[0] = 'Brasilia'
+print(cidades)
+
+# Adidicionar itens
+cidades.append('Santa Catarina')
+print(cidades)
+
+# Remover itens
+cidades.remove('Salvador')
+print(cidades)
+
+# Inserir itens
+cidades.insert(1, 'Santa Cataria')
+print(cidades)
+
+# Retirar itens
+cidades.pop(0)
+print(cidades)
+'''
+
+# Sort itens # Organiza em ordem alfabetica
+cidades.pop()
+print(cidades)
