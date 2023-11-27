162def308174431c319275265145da9e6a41cdf0
Altamirus <altamirus@hotmail.com>
2023-11-27 12:09:32 -0300

    Funcao_Lambda aula1

---
+++ Python_Udemy/funcao_lambda.py
@@ -0,0 +1,29 @@
+# Lambda Function
+# E uma funcao (pequena) sem nome
+# Pode ter varios argumentos mas somente 1 expressao
+# Muito ultilizado dentro de outras funcoes
+# codigo mais 'Clean'
+
+
+# Outro exemplo-1:
+def somar(x):
+    return x + 10
+
+
+print(somar(2))
+
+# Outro exemplo-2:
+
+
+def somar20(x): return x + 10
+
+
+print(somar20(4))
+
+# Outro exemplo-3:
+
+
+def somar30(x, y): return x + y * 2
+
+
+print(somar30(4, 6))
