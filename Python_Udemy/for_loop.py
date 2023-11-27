0b782926ad0d7335a4b1f20c6532f313201cdd36
Altamirus <altamirus@hotmail.com>
2023-11-26 21:21:47 -0300

    Tuples e listas diferenca ?

--- Python_Udemy/for_loop.py
+++ Python_Udemy/for_loop.py
@@ -8,9 +8,6 @@ for lin in range(linha):
     for c in range(coluna):
         print(simbolo, end='')
     print()
-'''
-
-
 # For loop dentro de uma lista
 
 valores = [50, 80, 110, 150, 170]
@@ -18,3 +15,17 @@ valores = [50, 80, 110, 150, 170]
 for x in valores:
 
     print(f' o valor final do produto e de R$ {x}.')
+# Lista de cores em estoque tem?
+
+cor_clientes = input('Digite a cor desejada: ')
+cores = ['Verde', 'Amarelo', 'Azul', 'Branco']
+
+if cor_clientes.lower() in cores:
+    print('Em estoque')
+else:
+    print('Nao temos em estoque')
+'''
+
+
+cores = ['Verde', 'Amarelo', 'Azul', 'Branco']
+cores = [10, 20, 30, 40,]
---
+++ Python_Udemy/tuples.py
@@ -0,0 +1,25 @@
+# Tuples
+# Armazenas mais de uma informacao em variaveis
+# Manter a sequencia dos dados em uma variavel
+# Nao podem ser alteradas (Immutable)
+
+
+cores_tuples = ('Verde', 'Amarelo', 'Azul', 'Branco')
+cores_list = ['Verde', 'Amarelo', 'Azul', 'Branco']
+
+
+print(cores_tuples)
+print(type(cores_list))
+print(type(cores_tuples))
+
+duas_listas = cores_list * 2
+duas_tuples = cores_tuples * 2
+print(duas_listas)
+print(duas_tuples)
+
+cores_list.append('Roxo')
+print(cores_list)
+
+# List sim e possivel inserir, remover, modificar
+# Tuples nao e possivel inserir, remover, modificar
+# Gera um erro -->cores_tuples.append('roxo')
