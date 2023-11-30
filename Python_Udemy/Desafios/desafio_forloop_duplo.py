4cbcb138ff404aa84912f220d94933044d379cbe
Altamirus <altamirus@hotmail.com>
2023-11-30 09:22:08 -0300

    Tarefa realizada com sucesso

---
+++ Python_Udemy/Desafios/desafio_forloop_duplo.py
@@ -0,0 +1,10 @@
+# For loop Iner e Outed
+# Faca combinacoes diferente com frutas e vegetais
+#
+
+frutas = ['Maca', 'laranja', 'banana', 'abacaxi']
+vegetais = ['cenoura', 'alface', 'brocole']
+
+for f in frutas:
+    for v in vegetais:
+        print(f, 'com', v)
