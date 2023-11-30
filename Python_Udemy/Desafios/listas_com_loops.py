972b8f787243b8f75bf75a7c80bdb74eb1177799
Altamirus <altamirus@hotmail.com>
2023-11-30 08:56:20 -0300

    Desafio executado com sucesso

---
+++ Python_Udemy/Desafios/listas_com_loops.py
@@ -0,0 +1,13 @@
+# Desafio for loop
+
+'''
+Para esse desafio, comece com a lista 'Frutas'
+Use um for loop para percorrer a lista e imprimir 
+cada fruta precedida pelo texo
+"Eu gosto de".
+'''
+
+frutas = ['Maca', 'laranja', 'banana', 'pera', 'uva', 'abaxaxi']
+
+for lista in frutas:
+    print('Eu gosto de' + lista)
